a7bba238efb374937fc84c3486ea801824bd73bc
Yolan Romailler <yolan@romailler.ch>
2016-05-10 11:01:00 +0200

    Adding a Makefile and enforcing format using gofmt

--- src/bindings/bindings.go
+++ src/bindings/bindings.go
@@ -1,13 +1,13 @@
-package main    
+package main
 
 import (
 	"bytes"
-    "flag"
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
-    "os"
 )
 
 var tiny_path string
@@ -25,87 +25,97 @@ func ExampleCommand() {
 }
 
 func endian(data string) string {
-        //initalizing the return value as an empty string
-        ans := ""
-        
-        //trimming since there are easily \n in cmd lines outputs.
-        data = strings.TrimSpace(data)
+	//initalizing the return value as an empty string
+	ans := ""
 
-        //if the data isn't in hex format, i.e. if if hasn't an even number of char, then the programmer made some mistake... Don't need to check if %2==0
-        for len(data)>=2{
-            ans+=data[len(data)-2:]
-            //fmt.Printf(ans+"\n")
-            data=data[:len(data)-2]
-        }
+	//trimming since there are easily \n in cmd lines outputs.
+	data = strings.TrimSpace(data)
 
-        return ans
+	//if the data isn't in hex format, i.e. if if hasn't an even number of char, then the programmer made some mistake... Don't need to check if %2==0
+	for len(data) >= 2 {
+		ans += data[len(data)-2:]
+		//fmt.Printf(ans+"\n")
+		data = data[:len(data)-2]
+	}
+
+	return ans
 }
 
 func alice(data string, port string) {
-    fmt.Printf("Alice here, running as server on port %s.\n",port)
-    out, err := exec.Command(tiny_path+"/bin/garbled_circuit/TinyGarble", "-a", "-i", tiny_path+"/scd/netlists/aes_1cc.scd" ,"--init" ,data[:32],"-p", port).Output()
+	fmt.Printf("Alice here, running as server on port %s.\n", port)
+	out, err := exec.Command(
+		tiny_path+"/bin/garbled_circuit/TinyGarble", "-a",
+		"-i", tiny_path+"/scd/netlists/aes_1cc.scd",
+		"--init", data[:32], "-p", port).Output()
+
 	if err != nil {
 		log.Fatal(err)
 	}
-    fmt.Printf("Alice output:  %s\n", out)
+	fmt.Printf("Alice output:  %s\n", out)
 }
 
-
 func bob(data string, addr string, port string) {
-    fmt.Printf("Bob here, running as client on address %s and port %s.\n", addr, port)
-    out, err := exec.Command(tiny_path+"/bin/garbled_circuit/TinyGarble", "-b", "-i", tiny_path+"/scd/netlists/aes_1cc.scd", "--init", data[:32],"-s", addr , "-p" ,port).Output()
+	fmt.Printf("Bob here, running as client on address %s and port %s.\n", addr, port)
+	out, err := exec.Command(
+		tiny_path+"/bin/garbled_circuit/TinyGarble", "-b",
+		"-i", tiny_path+"/scd/netlists/aes_1cc.scd",
+		"--init", data[:32], "-s", addr, "-p", port).Output()
+
 	if err != nil {
 		log.Fatal(err)
 	}
-    fmt.Printf("Bob output:  %s\n", endian(string(out)))
+	fmt.Printf("Bob output:  %s\n", endian(string(out)))
 }
 
 func main() {
 
-        nArgs := len(os.Args)
+	nArgs := len(os.Args)
 
-        if nArgs <= 1 {
-            log.Fatal("No argument, please run as server Alice (-a) first and then as Bob (-b). Use -help for help.")
-        }
+	if nArgs <= 1 {
+		log.Fatal("No argument, please run as server Alice (-a) first and then as Bob (-b). Use -h for help.")
+	}
 
-        rootPtr := flag.String("r",os.Getenv("TINYGARBLE"), "TinyGarble root directory path")
-        ports := flag.String("p", "1234", "Specify a starting ")
-        addrPtr :=  flag.String("s","127.0.0.1","Specify a server address for Bob to connect.")
+	rootPtr := flag.String("r", os.Getenv("TINYGARBLE"), "TinyGarble root directory path")
+	ports := flag.String("p", "1234", "Specify a starting ")
+	addrPtr := flag.String("s", "127.0.0.1", "Specify a server address for Bob to connect.")
 
-        alicePtr := flag.Bool("a", false, "Run as server Alice")
-        bobPtr := flag.Bool("b", false, "Run as client Bob")
+	alicePtr := flag.Bool("a", false, "Run as server Alice")
+	bobPtr := flag.Bool("b", false, "Run as client Bob")
 
-        initPtr := flag.String("d", "00000000000000000000000000000000", "Init data")
+	initPtr := flag.String("d", "00000000000000000000000000000000", "Init data")
 
-        flag.Parse()
+	flag.Parse()
 
-        // Checking the remaining flag used : if there are unknown flags, we stop.
-        if len(flag.Args()) > 0 {
-            log.Fatal("Please use the good arguments. Use -h for help.")
-        }
-        tiny_path = *rootPtr
+	// Checking the remaining flag used : if there are unknown flags, we stop.
+	if len(flag.Args()) > 0 {
+		log.Fatal("Please use the good arguments. Use -h for help.")
+	}
+	tiny_path = *rootPtr
 
-        if tiny_path == "" {
-                fmt.Printf("$TINYGARBLE not set. Assuming TinyGarble is located in $HOME/Code/TinyGarble\n")
-                home := os.Getenv("HOME")
-                if home != "" {
-                        tiny_path = home+"/Code/TinyGarble"
-                        // TODO: check if path exists (exec.Command does it, but it may be better to perform the check here)
-                } else {
-                        log.Fatal("$HOME is not set. Please provide path to TinyGarble's root as argument or set $TINYGARBLE env var to its path.")
-                }
-        }
+	if tiny_path == "" {
+		fmt.Printf("$TINYGARBLE not set. Assuming TinyGarble is located in $HOME/Code/TinyGarble\n")
+		home := os.Getenv("HOME")
+		if home != "" {
+			tiny_path = home + "/Code/TinyGarble"
+			// TODO: check if path exists (exec.Command does it, but it may be better to perform the check here)
+		} else {
+			log.Fatal("$HOME is not set. Please provide path to TinyGarble's root as argument or set $TINYGARBLE env var to its path.")
+		}
+	}
 
-        if !(*alicePtr) && !(*bobPtr) {
-        }
+	if !(*alicePtr) && !(*bobPtr) {
+	}
 
-        if len(*initPtr)<32 {
-            log.Fatal("Please give an init value of length 32 at least until I've implemented padding.")
-        }
+	if len(*initPtr) < 32 {
+		log.Fatal("Please give an init value of length 32 at least until I've implemented padding.")
+	}
 
-        switch {
-                case *alicePtr: alice(*initPtr,*ports)
-                case *bobPtr: bob(*initPtr,*addrPtr, *ports)
-                default : log.Fatal("Please run as server Alice (-a) first and then as Bob (-b). Use -h for help.")
-        }
+	switch {
+	case *alicePtr:
+		alice(*initPtr, *ports)
+	case *bobPtr:
+		bob(*initPtr, *addrPtr, *ports)
+	default:
+		log.Fatal("Please run as server Alice (-a) first and then as Bob (-b). Use -h for help.")
+	}
 }
