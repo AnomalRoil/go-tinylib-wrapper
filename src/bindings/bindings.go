7651e7a63ffaa2db8bc830f74322ab848ed24db7
Yolan Romailler <yolan@romailler.ch>
2016-05-10 14:38:15 +0200

    Changing bits, working now. Endianness reversed to be compatible with internet's test vectors

--- src/bindings/bindings.go
+++ src/bindings/bindings.go
@@ -11,6 +11,7 @@ import (
 )
 
 var tiny_path string
+var circuitPath string
 
 func ExampleCommand() {
 	cmd := exec.Command("tr", "a-z", "A-Z")
@@ -41,12 +42,20 @@ func endian(data string) string {
 	return ans
 }
 
-func alice(data string, port string) {
+func alice(data string, port string, clock_cycles int) {
 	fmt.Printf("Alice here, running as server on port %s.\n", port)
+	// Note the change of endianness for the data
+	data = endian(data)
+
+	inputArg := "--input"
+	if clock_cycles > 1 {
+		inputArg = "--init"
+	}
+	fmt.Println(tiny_path, circuitPath, inputArg, data, port)
 	out, err := exec.Command(
 		tiny_path+"/bin/garbled_circuit/TinyGarble", "-a",
-		"-i", tiny_path+"/scd/netlists/aes_1cc.scd",
-		"--init", data[:32], "-p", port).Output()
+		"-i", circuitPath,
+		inputArg, data[:32], "-p", port).Output()
 
 	if err != nil {
 		log.Fatal(err)
@@ -54,13 +63,21 @@ func alice(data string, port string) {
 	fmt.Printf("Alice output:  %s\n", out)
 }
 
-func bob(data string, addr string, port string) {
+func bob(data string, addr string, port string, clock_cycles int) {
 	fmt.Printf("Bob here, running as client on address %s and port %s.\n", addr, port)
+	// Note the change of endianness for the data
+	data = endian(data)
+
+	inputArg := "--input"
+	if clock_cycles > 1 {
+		inputArg = "--init"
+	}
+
 	out, err := exec.Command(
 		tiny_path+"/bin/garbled_circuit/TinyGarble", "-b",
-		"-i", tiny_path+"/scd/netlists/aes_1cc.scd",
-		"--init", data[:32], "-s", addr, "-p", port).Output()
-
+		"-i", circuitPath,
+		inputArg, data[:32], "-s", addr, "-p", port, "--output_mode", "2").Output()
+	// We specify the --output_mode arg to be "last_clock" only, since otherwise it would output each clock cycle intermediate states.
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -75,8 +92,12 @@ func main() {
 		log.Fatal("No argument, please run as server Alice (-a) first and then as Bob (-b). Use -h for help.")
 	}
 
-	rootPtr := flag.String("r", os.Getenv("TINYGARBLE"), "TinyGarble root directory path")
-	ports := flag.String("p", "1234", "Specify a starting ")
+	rootPtr := flag.String("r", os.Getenv("TINYGARBLE"), "TinyGarble root directory path, default to $TINYGARBLE if var set, writes $TINYGARBLE if changed")
+	circuitPtr := flag.String("n", "aes_1cc.scd", "name of the circuit file located in the circuit root directory")
+	circuitPathPtr := flag.String("c", "$TINYGARBLE/scd/netlists", "location of the circuit root directory. Default : $TINYGARBLE/scd/netlists")
+	clockcyclesPtr := flag.Int("cc", 1, "number of clock cycles needed for this circuit, usually 1, usually indicated at the end of the circuit name, sha3_24cc needs 24 clock cycles for example")
+
+	portsPtr := flag.String("p", "1234", "Specify a starting port")
 	addrPtr := flag.String("s", "127.0.0.1", "Specify a server address for Bob to connect.")
 
 	alicePtr := flag.Bool("a", false, "Run as server Alice")
@@ -90,8 +111,8 @@ func main() {
 	if len(flag.Args()) > 0 {
 		log.Fatal("Please use the good arguments. Use -h for help.")
 	}
-	tiny_path = *rootPtr
 
+	tiny_path = *rootPtr
 	if tiny_path == "" {
 		fmt.Printf("$TINYGARBLE not set. Assuming TinyGarble is located in $HOME/Code/TinyGarble\n")
 		home := os.Getenv("HOME")
@@ -103,8 +124,13 @@ func main() {
 		}
 	}
 
-	if !(*alicePtr) && !(*bobPtr) {
+	circuitPath = *circuitPathPtr + "/"
+	// We convert $TINYGARBLE into tiny_path, mainly because of the default value includes it.
+	if strings.Contains(circuitPath, "$TINYGARBLE") {
+		circuitPath = strings.Replace(circuitPath, "$TINYGARBLE", tiny_path, -1)
 	}
+
+	circuitPath += *circuitPtr
 
 	if len(*initPtr) < 32 {
 		log.Fatal("Please give an init value of length 32 at least until I've implemented padding.")
@@ -112,10 +138,10 @@ func main() {
 
 	switch {
 	case *alicePtr:
-		alice(*initPtr, *ports)
+		alice(*initPtr, *portsPtr, *clockcyclesPtr)
 	case *bobPtr:
-		bob(*initPtr, *addrPtr, *ports)
-	default:
+		bob(*initPtr, *addrPtr, *portsPtr, *clockcyclesPtr)
+	default: // if running neither as Alice, nor as Bob, there is a misuse
 		log.Fatal("Please run as server Alice (-a) first and then as Bob (-b). Use -h for help.")
 	}
 }
